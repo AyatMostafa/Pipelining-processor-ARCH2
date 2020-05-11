787d1f5a998579ff6694368337b9aad5e99a2916
AyatMostafa <45332061+AyatMostafa@users.noreply.github.com>
2020-05-11 21:47:19 +0200

    Update Hazard_Load_case.vhd

--- phase 3/Decode/Hazard_Load_case.vhd
+++ phase 3/Decode/Hazard_Load_case.vhd
@@ -3,7 +3,7 @@ USE IEEE.std_logic_1164.all;
 
 ENTITY Hazard_Load_case IS  
 	PORT (  ID_EX_MemRead : std_logic; 
-		ID_EX_Rt, IF_ID_S1, IF_ID_S2:  IN std_logic_vector(31 downto 0);
+		ID_EX_Rt, IF_ID_S1, IF_ID_S2:  IN std_logic_vector(2 downto 0);
   		Hazard   : OUT  std_logic
 		);    
 END ENTITY Hazard_Load_case;
