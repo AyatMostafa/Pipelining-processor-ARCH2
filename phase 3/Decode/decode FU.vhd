3ebc4839b1a52d14a7dea61186ca52dcbe6bc7c1
a7med7amdy <32201440+a7med7amdy@users.noreply.github.com>
2020-05-19 22:57:07 +0200

    correcting forwarding unit at decode

--- phase 3/Decode/decode FU.vhd
+++ phase 3/Decode/decode FU.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 entity DFU is
 Port ( 
-clk : IN std_logic;
+--clk : IN std_logic;
 enable : in std_logic;
 Rdest_JZ : in STD_LOGIC_VECTOR (2 downto 0);
 Rdst_exec : in STD_LOGIC_VECTOR (2 downto 0);
@@ -18,7 +18,7 @@ end DFU;
 
 architecture Behavioral of DFU is 
 
-signal mx1 : STD_LOGIC_VECTOR (1 downto 0):="00";
+--signal mx1 : STD_LOGIC_VECTOR (1 downto 0):="00";
 
 begin
 
@@ -28,29 +28,29 @@ PROCESS(clk) IS
 BEGIN
 
 if enable = '1' then
-   IF rising_edge(clk) THEN
+   --IF rising_edge(clk) THEN
 
      IF wb_exec = '1' and (Rdest_JZ(0)=Rdst_exec(0) and Rdest_JZ(1)=Rdst_exec(1) and Rdest_JZ(2)=Rdst_exec(2)) THEN
      
-        mx1 <= "10";
+        mux1 <= "10";
 
      elsif wb_mem = '1' and  (Rdest_JZ(0)=Rdst_mem(0) and Rdest_JZ(1)=Rdst_mem(1) and Rdest_JZ(2)=Rdst_mem(2)) THEN
 
-         mx1 <= "01";
+         mux1 <= "01";
 
 
      else
 
-       mx1 <= "00";
+       mux1 <= "00";
 
      END IF;
 
-   END IF;
+   --END IF;
 
 else
-   mx1<="00";
+   mux1<="00";
 end if;
-mux1<= mx1;
+--mux1<= mx1;
 
 END PROCESS; 
 
